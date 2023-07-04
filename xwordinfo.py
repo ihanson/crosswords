c856d80f53c3fbe640e43a33e9e62ab3bbac76a1
Ira Hanson <hanson.ira@gmail.com>
2023-07-04 14:10:17 -0500

    Add scraper for XWord Info acrostics

--- xwordinfo.py
+++ xwordinfo.py
@@ -1,9 +1,14 @@
 import datetime
 import requests
 import os
+import json
+import html
+import string
+import re
 from bs4 import BeautifulSoup
 import crossword
 import jpz
+import acrostic
 
 def download_puzzle(type: str, date: datetime.date) -> crossword.Puzzle:
 	soup = BeautifulSoup(
@@ -79,3 +84,46 @@ def save_variety(date: datetime.date):
 		f"puzzles\\Variety_{date.isoformat()}.jpz"
 	)
 
+def download_acrostic(date: datetime.date) -> acrostic.Acrostic:
+	page_url = f"https://www.xwordinfo.com/Acrostic?date={format_date(date)}"
+	soup = BeautifulSoup(
+		requests.get(
+			page_url
+		).text,
+		"html.parser"
+	)
+	title = soup.find("h1").get_text()
+	author = re.match(r"^by (.*?), edited", soup.find_all("h2")[0].get_text())[1]
+	data = json.loads(
+		requests.get(
+			f"https://www.xwordinfo.com/JSON/AcrosticData.ashx?date={format_date(date)}",
+			headers={"Referer": page_url}
+		).text
+	)
+	quote_with_attr = data["quote"]
+	quote_match = re.match(r"^(.*?),\s*(.*?)\s*(?:--|\u2014)\s*(.*)$", html.unescape(quote_with_attr))
+	if quote_match is None:
+		raise Exception(f"Could not parse quote: {quote_with_attr}")
+	return acrostic.Acrostic(
+		squares=[
+			(
+				acrostic.LetterSquare(
+					answer=answer_letter,
+					clue_index=string.ascii_uppercase.index(grid_letter),
+					clue_word_index=clue_word_index
+				) if grid_letter in string.ascii_uppercase
+				else acrostic.PunctuationSquare(answer_letter)
+			) for (answer_letter, grid_letter, clue_word_index) in zip(
+				data["answerKey"],
+				data["gridLetters"],
+				(i - 1 if i > 0 else None for i in data["cluePos"])
+			)
+		],
+		clues=[html.unescape(clue) for clue in data["clues"]],
+		quote_text=quote_match[3],
+		quote_author=quote_match[1],
+		quote_work=quote_match[2],
+		title=title,
+		author=author,
+		copyright=data["copyright"]
+	)
